9f2cb35f073c587cac5440742467af1e0d706255
vedantpawar18 <vedantpawar18@gmail.com>
2022-09-07 08:33:41 +0530

    morgan logger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan= require("morgan");
 const app= express();
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(":method :status :res[content-length] :response-time ms :date HTTP/:http-version :url "))
+app.use(morgan(":method :status :res[content-length] :response-time ms :date HTTP/:http-version :url /n"))
 
 app.get("/", (req,res)=>{
     res.send("homepage")
